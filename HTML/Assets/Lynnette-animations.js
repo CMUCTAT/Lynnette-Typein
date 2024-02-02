6ef1c6ea57fd9a2851b99289dbb58a091d941bc7
MaxDBenson <mbenson1994@gmail.com>
2024-02-02 18:27:46 -0500

    change problem statement text, add message on autocomplete, update masteredSkills per step, refactor Lynnette-animations.js

--- HTML/Assets/Lynnette-animations.js
+++ HTML/Assets/Lynnette-animations.js
@@ -1,4 +1,60 @@
 var currentRow = 0
+var pathSelected = null;
+var masteredSkills;
+
+const AUTO_SOLVE_MSG = "Because you have demonstrated the knowledge needed to complete the remaining steps, I have finished the problem for you.  You can review what I've done, or click \"Finish Problem\" to advance."
+
+
+function takeStep(step) {
+  let selection = step.selection;
+  let action = "UpdateTextField";
+  let input = step.input;
+  let ctatSAI = new CTATSAI(selection, action, input),
+  stepType = "ATTEMPT";
+  console.log(
+    "\tsend sai: " +
+      selection +
+      "," +
+      action +
+      "," +
+      input +
+      ", (" +
+      "tutored)"
+  );
+  CTATCommShell.commShell.processComponentAction(
+    ctatSAI, //sai
+    true, //tutored
+    true, //behavior recorded
+    null, //[deprecated]
+    stepType, //log type
+    null, //aTrigger
+  );
+}
+
+function getFieldNumber(field) {
+  return Number(field.getName().replace("solve",""));
+}
+
+function assignSelections(path) {  
+  let fields = CTATShellTools.getAllComponents().filter((c)=>c.getClassName() === "CTATMathInput" && !c.getValue())
+    .sort((a, b)=>getFieldNumber(a) - getFieldNumber(b));
+  path.forEach((step, idx) => {
+    step.selection = fields[idx].getName();
+  });
+}
+
+function followPath(path) {
+  console.log("followPath: ",path);
+  assignSelections(path);
+  path.forEach(takeStep);
+}
+
+function updateMasteredSkills() {
+  let allSkills = CTAT.ToolTutor.tutor.getProblemSummary().getSkills().toJSONforTutorshop();
+  masteredSkills = allSkills.filter((s)=> s.p_known >= 0.95);
+  console.log("all skills are ",allSkills.map((s)=>s.name));
+  console.log("mastered skills are ",masteredSkills.map((s)=>s.name));
+}
 
 function showNextRow() {
   document.getElementById(`solve${++currentRow}Group`).parentNode.style.display = 'flex'
@@ -6,30 +62,12 @@ function showNextRow() {
 
 tutorInitializer.then(() => {
   updateDone()
-  addFeedbackListener()
-  addHintWindowListener()
-  // adjustSkills()
-  // addSkillListener()
+  addMessageListener();
+  updateMasteredSkills();
 })
 
 function updateDone() {
   document.getElementById('doneButton').getElementsByClassName('CTAT-done-button--text')[0].innerHTML = 'Finish Problem'
-}
-
-function addFeedbackListener() {
-  CTATCommShell.commShell.addGlobalEventListener({
-    processCommShellEvent: function(event, message) {
-      // console.log('EVENT', event, 'MESSAGE', message)
-      if (event == 'AssociatedRules' && message) {
-        let indicator = message.getIndicator(),
-            sai = message.getSAI(),
-            selection = (sai ? sai.getSelection() : '_noSuchComponent_'),
-            selectionElement = document.getElementById(selection)
-        if (selectionElement && selectionElement.classList.contains('animate'))
-          window.setTimeout(delayIndicator, 700, selection, indicator)
-      }
-    }
-  })
 }
 
 function delayIndicator(selection, indicator) {
@@ -50,94 +88,76 @@ function delayIndicator(selection, indicator) {
   else indicatorBox.replaceChild(newIndicatorElement, currentIndicatorElement)
 }
 
-function addHintWindowListener() {
+function handleAssociatedRules(message) {
+  if(message) {
+    
+    let indicator = message.getIndicator(),
+      sai = message.getSAI(),
+      selection = (sai ? sai.getSelection() : '_noSuchComponent_'),
+      selectionElement = document.getElementById(selection)
+    
+    if (pathSelected && (selection === pathSelected[pathSelected.length-1].selection)) {
+      //last step, show message
+      console.log("last step");
+      document.getElementById('HintWindow').style.display = 'flex';
+      CTATShellTools.showHints([AUTO_SOLVE_MSG]);
+    } else {
+      document.getElementById('HintWindow').style.display = message.getProperty('TutorAdvice') ? 'flex' : 'none'
+    }
+    
+    if (selectionElement && selectionElement.classList.contains('animate')) {
+      window.setTimeout(delayIndicator, 700, selection, indicator)
+    }
+  }
+}
+
+function handleInterfaceAction(message) {
+  if (message && typeof message == 'object') {
+    let sai = message.getSAI(),
+        selection = (sai ? sai.getSelection() : '_noSuchComponent_')
+    if (sai.getAction() == 'SetVisible') {
+      document.getElementById(selection).style.display = 'flex'
+    }
+  }
+}
+
+function handleCorrectAction(message) {
+  console.log("correct action, solution paths are now: ",window._solutionPaths);
+  const masteredPaths = [];
+  if (window._solutionPaths && !pathSelected) {
+    window._solutionPaths.forEach((sp, idx) => {
+      const skills = [...new Set(sp.map((s)=>s.skill))];
+      console.log("\tskills for path "+idx+": ",skills);
+      if (skills.every((s)=>masteredSkills.find(ms => ms.name === s))) {
+        console.log("\tall skills mastered for this path");
+        masteredPaths.push(sp);
+      }
+    });
+
+    if (masteredPaths.length === window._solutionPaths.length) {
+      console.log("no paths with unmastered skills");
+      pathSelected = masteredPaths[0];
+      followPath(masteredPaths[0]);
+    }
+  }
+}
+
+function addMessageListener() {
   CTATCommShell.commShell.addGlobalEventListener({
     processCommShellEvent: function(event, message) {
-      if (event == 'AssociatedRules' && message)
-        document.getElementById('HintWindow').style.display = message.getProperty('TutorAdvice') ? 'flex' : 'none'
-      if (event == 'InterfaceAction' && message && typeof message == 'object') {
-        let sai = message.getSAI(),
-            selection = (sai ? sai.getSelection() : '_noSuchComponent_')
-        if (sai.getAction() == 'SetVisible')
-          document.getElementById(selection).style.display = 'flex'
+      switch (event) {
+        case 'CorrectAction':
+          handleCorrectAction(message);
+        case 'InCorrectAction':
+          updateMasteredSkills();
+          break;
+        case 'AssociatedRules': 
+          handleAssociatedRules(message);
+          break;
+        case 'InterfaceAction':
+          handleInterfaceAction(message);
+          break;
       }
     }
-  })
+  });
 }
-
-function addSkillListener() {
-  CTATCommShell.commShell.addGlobalEventListener({
-    processCommShellEvent: function(event) {
-      if (event == 'ProblemRestoreEnd') {
-        console.log('ProblemRestoreEnd')
-        adjustSkills()
-      }
-    }
-  })
-}
-
-function adjustSkills() {
-  console.log('adjustSkills')
-  Array.prototype.forEach.call(document.getElementsByClassName('CTATSkillWindow--bar'), skillBarElement => {
-    let initialElement = document.createElement('div')
-    initialElement.classList.add('CTATSkillWindow--initial')
-    initialElement.style.width = skillBarElement.firstChild.style.width
-    skillBarElement.append(initialElement)
-  })
-}
-
-// function addSkillListener() {
-//   let initialSkills = []
-//   CTATCommShell.commShell.addGlobalEventListener({
-//     processCommShellEvent: function(event, message) {
-//       if (event == 'ProblemRestoreEnd') {
-//         console.log('ProblemRestoreEnd')
-//         window.message = message
-//         initialSkills = message.getSkillsObject().getSkillSet()
-//         setInitialSkills(initialSkills)
-//       } else if (event == 'AssociatedRules' && message) {
-//         console.log('AssociatedRules')
-//         window.message = message
-//         let atLeastOne = false,
-//             skillElementLabels = document.getElementsByClassName('CTATSkillWindow--label')
-//         message.getSkillsObject().getSkillSet().forEach(skill => {
-//           let label = skill.getSkillName(),
-//               labelElement = skillElementLabels.find(labelElement => labelElement.innerHTML == label)
-//           if (labelElement) {
-//             atLeastOne = true
-//             labelElement.parentNode.classList.add('focusProgress')
-//           }
-//         })
-//         setInitialSkills(initialSkills)
-//         if (atLeastOne) {
-//           skillElementLabels.forEach(labelElement => {
-//             let parentElement = labelElement.parentNode
-//             if (!parentElement.classList.contains('focusProgress')) parentElement.classList.add('blurProgress')
-//           })
-//           window.setTimeout(() => {
-//             document.getElementsByClassName('CTATSkillWindow--skill').foreach(skillBar =>
-//               skillBar.classList.remove('focusProgress', 'blurProgress')
-//             )
-//           }, 2000)
-//         }
-//       }
-//     }
-//   })
-// }
-
-// function setInitialSkills(skills) {
-//   let skillElementLabels = document.getElementsByClassName('CTATSkillWindow--label')
-//   skills.forEach(skill => {
-//     let label = skill.getSkillName(),
-//         level = skill.getLevel(),
-//         labelElement = skillElementLabels.forEach(labelElement => labelElement.innerHTML == label)
-//     if (labelElement) {
-//       let focus = labelElement.previousSibling,
-//           width = focus.offsetWidth,
-//           element = document.createElement('div')
-//       element.classList.add('CTATSkillWindow--initial')
-//       element.style.width = level * width + 'px'
-//       focus.firstChild.append(element)
-//     }
-//   })
-// }
